9cb51961cdc5ebb325ae638777e8c869cab28d9d
Artem Vol <artemvol1998@gmail.com>
2025-02-26 13:10:41 +0300

    Feature: init django project. Add docker files. Add start.sh file. Add poetry dependencies.

---
+++ user_service/bot_management/settings.py
@@ -0,0 +1,119 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+load_dotenv(BASE_DIR / ".env")
+
+SECRET_KEY = os.getenv(
+    "USER_SERVICE_SECRET_KEY", "DSAmdU3H783hs8M9S30k9xSi9d3KD"
+)
+
+DEBUG = bool(int(os.getenv("USER_SERVICE_DJANGO_DEBUG", 1)))
+DEVELOPMENT = bool(int(os.getenv("USER_SERVICE_DJANGO_DEVELOPMENT", 1)))
+
+ALLOWED_HOSTS = [
+    os.getenv("USER_SERVICE_ALLOWED_HOST", "*"),
+]
+
+# Application definition
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "bot_management.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "bot_management.wsgi.application"
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": os.getenv(
+            "USER_SERVICE_DB_ENGINE", "django.db.backends.sqlite3"
+        ),
+        "HOST": os.getenv("USER_SERVICE_DB_HOST"),
+        "PORT": os.getenv("USER_SERVICE_DB_PORT"),
+        "USER": os.getenv("USER_SERVICE_DB_USER"),
+        "PASSWORD": os.getenv("USER_SERVICE_DB_PASSWORD"),
+        "NAME": os.getenv("USER_SERVICE_DB_DB", BASE_DIR / "db.sqlite3"),
+    }
+}
+
+# Password validation
+# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.1/topics/i18n/
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
