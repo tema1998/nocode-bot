7352342afa7c02a402807c2bac535b4787c47aad
Artem Vol <artemvol1998@gmail.com>
2025-03-06 10:25:23 +0300

    Refactor: translate comments to eng.

--- user_service/static/bootstrap/js/bot-details.js
+++ user_service/static/bootstrap/js/bot-details.js
@@ -1,12 +1,16 @@
+// Attach an event listener to the button with the ID 'showTokenBtn'
 document.getElementById('showTokenBtn').addEventListener('click', function() {
-    var tokenElement = document.getElementById('tokenInput'); // исправленное имя идентификатора
+    // Get the input element with the ID 'tokenInput'
+    var tokenElement = document.getElementById('tokenInput'); // Corrected identifier name
+    // Reference to the button that triggered the event
     var button = this;
 
+    // Check if the token input element is currently hidden
     if (tokenElement.style.display === 'none') {
-        tokenElement.style.display = 'inline'; // показываем поле
-        button.textContent = 'Скрыть';
+        tokenElement.style.display = 'inline'; // Show the input field
+        button.textContent = 'Скрыть'; // Change button text to 'Hide'
     } else {
-        tokenElement.style.display = 'none'; // скрываем поле
-        button.textContent = 'Показать';
+        tokenElement.style.display = 'none'; // Hide the input field
+        button.textContent = 'Показать'; // Change button text to 'Show'
     }
 });
