39cfbc3d2e0068a10a3491d6e09cfeb723c0d65e
Artem Vol <artemvol1998@gmail.com>
2025-04-03 06:31:52 +0300

    Fix(bot-service): - forbid button name "/update".

--- bot_service/services/main_menu_service.py
+++ bot_service/services/main_menu_service.py
@@ -302,7 +302,7 @@ class MainMenuService:
                 status_code=400,
                 detail="A button with the same text already exists.",
             )
-        if button_text == "/start":
+        if button_text == "/start" or button_text == "/update":
             raise HTTPException(
                 status_code=400,
                 detail="A button with this name is forbidden.",
