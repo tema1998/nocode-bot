7046e945c69913b8cd70b84b759495ff6c22ab77
Artem Vol <artemvol1998@gmail.com>
2025-04-06 08:29:40 +0300

    Feature: - add and configure nginx.

--- user_service/bot_management/settings/static.py
+++ user_service/bot_management/settings/static.py
@@ -4,8 +4,10 @@ from .base import BASE_DIR
 
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # Куда собирать статику
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  # Доп. директории со статикой
+]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
