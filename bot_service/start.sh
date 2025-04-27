f094701d38b41ef2018dd46c048d65f15f1fc030
Artem Vol <artemvol1998@gmail.com>
2025-04-27 14:05:44 +0300

    Fix(bot-service): - add workers.

--- bot_service/start.sh
+++ bot_service/start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd bot_service && alembic upgrade head && cd ..
-gunicorn -k uvicorn.workers.UvicornWorker bot_service.main:app --bind 0.0.0.0:8080 --reload
+gunicorn -k uvicorn.workers.UvicornWorker bot_service.main:app --bind 0.0.0.0:8080 --workers 3 --reload
